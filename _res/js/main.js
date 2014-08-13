af70b247765dd91bb291984cfd9f9bb96dd8b00a
Jason Conny <jasonconny@gmail.com>
2014-08-13 11:10:58 -0500

    added JSON for first episode and some data bindings

--- _res/js/main.js
+++ _res/js/main.js
@@ -1,4 +1,30 @@
-function EpisodeController($scope) {
-    $scope.episodeNumber = "1";
-    $scope.episodeTitle = "Beginnings";
-}
+var hotCan = angular.module('hotCan', []);
+
+hotCan.controller('EpisodeController', ['$scope', '$http', function($scope, $http) {
+    $scope.url = '_res/json/001.json';
+
+
+    $http({method: 'GET', url: $scope.url}).
+        success(function(data, status) {
+
+            $scope.data = data;
+
+            $scope.episodeNumber = data.number;
+            $scope.episodeTitle = data.title;
+
+            $scope.introArtist = data.intro.artist;
+            $scope.introTitle = data.intro.title;
+            $scope.introAlbum = data.intro.album;
+            $scope.introAlbumURL = data.intro.albumURL;
+            $scope.introLabel = data.intro.label;
+            $scope.introYear = data.intro.year;
+
+        }).
+        error(function(data, status) {
+            console.log('get error: ' + status);
+        });
+
+}]);
+
+
+
