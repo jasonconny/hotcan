24313cebd0ba6bd761cb02159dd0e27b6efdf915
Jason Conny <jasonconny@gmail.com>
2014-08-26 17:19:50 -0500

    started adding routes

--- _res/js/main.js
+++ _res/js/main.js
@@ -1,7 +1,17 @@
+// hack to get root path during local development without running a web server
 var scripts = document.getElementsByTagName("script");
 var tmplPath = scripts[scripts.length-1].src.replace('js/main.js', 'tmpl/');
 
 var hotCan = angular.module('hotCan', []);
+
+//hotCan.config(function ($routeProvider) {
+//    $routeProvider.when('/about/',
+//        {
+//            templateUrl: tmplPath + "about.html",
+//            controller: "aboutController"
+//        }
+//    )
+//});
 
 hotCan.factory('Episode', ['$http', function($http) {
     var url = '_res/json/001.json'; // this needs to be passed in when service is called
@@ -10,6 +20,12 @@ hotCan.factory('Episode', ['$http', function($http) {
         getEpisode: function() {
             return $http.get(url);
         }
+    }
+}]);
+
+hotCan.controller('AboutController', ['$scope', function($scope) {
+    $scope.model = {
+        aboutText: "Two DJ’s and four turntables. Available for art openings, loft parties, club dates and bar mitzvahs. Hot Can is Jason Conny and Xavier Jimenez. Xavier Jimenez and Jason Conny are Hot Can. "
     }
 }]);
 
