7a8e5b5a56ce7072b5ceb8120b11f5656f772ba0
jasonconny <jasonconny@gmail.com>
2014-09-02 15:38:49 -0500

    added angular-route.js

--- _res/js/main.js
+++ _res/js/main.js
@@ -1,18 +1,23 @@
-// hack to get root path during local development without running a web server
-var scripts = document.getElementsByTagName("script");
-var tmplPath = scripts[scripts.length-1].src.replace('js/main.js', 'tmpl/');
+var hotCan = angular.module('hotCan', ['ngRoute']);
 
-var hotCan = angular.module('hotCan', []);
 
-//hotCan.config(function ($routeProvider) {
-//    $routeProvider.when('/about/',
-//        {
-//            templateUrl: tmplPath + "about.html",
-//            controller: "aboutController"
-//        }
-//    )
-//});
+// ROUTES
+hotCan.config(function ($routeProvider) {
+    $routeProvider.when('/',
+        {
+            templateUrl: "_res/views/episode.html"
+        }
+    )
+});
 
+// CONTROLLERS
+hotCan.controller('EpisodeController', ['$scope', 'Episode', function($scope, Episode) {
+    Episode.getEpisode().success(function(data) {
+        $scope.episode = data;
+    });
+}]);
+
+// SERVICES
 hotCan.factory('Episode', ['$http', function($http) {
     var url = '_res/json/001.json'; // this needs to be passed in when service is called
 
@@ -23,22 +28,11 @@ hotCan.factory('Episode', ['$http', function($http) {
     }
 }]);
 
-hotCan.controller('AboutController', ['$scope', function($scope) {
-    $scope.model = {
-        aboutText: "Two DJ’s and four turntables. Available for art openings, loft parties, club dates and bar mitzvahs. Hot Can is Jason Conny and Xavier Jimenez. Xavier Jimenez and Jason Conny are Hot Can. "
-    }
-}]);
-
-hotCan.controller('EpisodeController', ['$scope', 'Episode', function($scope, Episode) {
-    Episode.getEpisode().success(function(data) {
-        $scope.episode = data;
-    });
-}]);
-
-hotCan.directive('episode', function() {
-   return {
-       restrict: "E",
-       replace: true,
-       templateUrl: tmplPath + "episode.html"
-   }
-});
+// DIRECTIVES
+//hotCan.directive('episode', function() {
+//   return {
+//       restrict: "E",
+//       replace: true,
+//       templateUrl: "_res/views/episode.html"
+//   }
+//});
