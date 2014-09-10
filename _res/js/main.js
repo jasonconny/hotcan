cb58e0e599c57e48ad39de3f3433e14483d37ba0
jasonconny <jasonconny@gmail.com>
2014-09-09 22:23:08 -0500

    wiring up resource

--- _res/js/main.js
+++ _res/js/main.js
@@ -1,4 +1,4 @@
-var hotcan = angular.module('hotcan', ['ngRoute']);
+var hotcan = angular.module('hotcan', ['ngRoute', 'ngResource']);
 
 
 // ROUTES
@@ -6,11 +6,12 @@ hotcan.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
             controller: "EpisodeController",
-            templateUrl: "_res/views/episode.html"
-        })
-        .when('/podcast/:deeplink', {
-            controller: "EpisodeController",
-            templateUrl: "_res/views/episode.html"
+            templateUrl: "_res/views/all.html",
+            resolve: {
+                episodes: ['EpisodeLoader', function(EpisodeLoader) {
+                    return EpisodeLoader.query();
+                }]
+            }
         })
         .when('/about', {
             templateUrl: "_res/views/about.html"
@@ -18,26 +19,32 @@ hotcan.config(function ($routeProvider) {
         .when('/contact', {
             templateUrl: "_res/views/contact.html"
         })
+        .when('/podcast/:deeplink', {
+            redirectTo: "/:id"
+        })
+        .when('/:deeplink', {
+            controller: "EpisodeController",
+            templateUrl: "_res/views/episode.html"
+        })
 });
 
 // CONTROLLERS
-hotcan.controller('EpisodeController', ['$scope', 'EpisodeLoader', function($scope, EpisodeLoader) {
-    EpisodeLoader.success(function(data) {
-        var index = 0;
-        episodes = data;
-        $scope.currentEpisode = episodes[index];
-        $scope.previousEpisode = episodes[index - 1];
-        $scope.nextEpisode = episodes[index + 1];
-        console.log($scope.currentEpisode.date);
-        $scope.postDate = new Date($scope.currentEpisode.date);
-        console.log($scope.postDate);
+hotcan.controller('EpisodeController', ['$scope', 'episodes', function($scope, episodes) {
+    $scope.episodes = episodes;
+//    console.log(currentEpisode);
+//    var index = 0;
+//    $scope.currentEpisode = episodes[index];
+//    $scope.postDate = new Date($scope.currentEpisode.date);
+}]);
+
+
+// SERVICES
+hotcan.factory('EpisodeLoader', ['$resource', function($resource) {
+    return $resource('_res/json/hotcan.json', {}, {
+        query: {method:'GET', isArray:true}
     });
 }]);
 
-// SERVICES
-hotcan.factory('EpisodeLoader', ['$http', function($http) {
-    return $http.get('_res/json/hotcan.json');
-}]);
 
 // DIRECTIVES
 // episode-nav directive
