f6f2b6a2fa98544f32c54883aeab6c1e37863ad4
Jason Conny <jason.conny@leoburnett.com>
2015-05-27 21:46:16 -0500

    added watch task to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['watch']);
 };
